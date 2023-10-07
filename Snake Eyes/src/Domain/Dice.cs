5e161b4eeb12f5f0f72582bb255d0b81ab388191
Robin Verplancke <robin.verplancke@student.hogent.be>
2023-10-07 20:28:31 +0200

    add snake project

---
+++ Snake Eyes/src/Domain/Dice.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain;
+
+public class Dice
+{
+    #region Field
+    private Random _randomizer = new Random();
+    #endregion
+
+    #region Properties
+    public int Dots { get; set; }
+    #endregion
+
+    #region Constructors
+    public Dice()
+    {
+        Dots = 6;
+    }
+    #endregion
+
+    #region Methods
+    public void Roll()
+    {
+        Dots = _randomizer.Next(1, 7);
+    }
+    #endregion
+}
---
+++ Snake Eyes/src/Domain/Game.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain;
+
+public class Game
+{
+    #region Fields
+    private List<int> _highscores = new List<int>();
+    private Dice _dice1;
+    private Dice _dice2;
+    #endregion
+
+    #region Properties
+    public int Eye1 => _dice1.Dots;
+    public int Eye2 => _dice2.Dots;
+    public bool HasSnakeEyes { get; set; }
+    public IReadOnlyList<int> HighScores => _highscores.AsReadOnly();
+    public int Total { get; set; }
+    #endregion
+
+    #region Constructors
+    public Game()
+    {
+        Initialize();
+    }
+    #endregion
+
+    #region Methods
+    private void Initialize()
+    {
+        _dice1 = new Dice();
+        _dice2 = new Dice();
+        HasSnakeEyes = false;
+    }
+
+    public void Play()
+    {
+        _dice1.Roll();
+        _dice2.Roll();
+     
+        if (Eye1 == 1 && Eye2 == 1)
+        {
+            HasSnakeEyes = true;
+            _highscores.Add(Total);
+            Total = 0;
+        } else
+        {
+            Total += Eye1 + Eye2;
+        }
+    }
+
+    public void Restart()
+    {
+        Initialize();
+        Play();
+    }
+    #endregion
+}
